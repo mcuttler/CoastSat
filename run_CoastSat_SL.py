4f1b74d888aa994aa1bcacdedbc8fb1d63c86608
mcuttler <michael.cuttler@uwa.edu.au>
2019-05-21 15:17:22 +0800

    update for running different locations

--- run_CoastSat_SL.py
+++ run_CoastSat_SL.py
@@ -23,16 +23,16 @@ from coastsat import SDS_download, SDS_preprocess, SDS_shoreline, SDS_tools, SDS
 #            [151.301454, -33.700754]]]
 # can also be loaded from a .kml polygon
 #kml_polygon = os.path.join(os.getcwd(), 'examples', 'NARRA_polygon.kml')
-polygon = SDS_tools.polygon_from_kml(os.path.join(os.getcwd(), 'KMLs','TANTABIDDI_SALIENT.kml'))
+polygon = SDS_tools.polygon_from_kml(os.path.join(os.getcwd(), 'KMLs','CORAL_BAY.kml'))
        
 # date range
-dates = ['2019-01-01', '2019-05-01']
+dates = ['1986-01-01', '2019-05-01']
 
 # satellite missions
-sat_list = ['S2']
+sat_list = ['L5','L7','L8','S2']
 
 # name of the site
-sitename = 'TANTABIDDI_SALIENT'
+sitename = 'CORAL_BAY'
 
 # filepath where data will be stored
 filepath_data = os.path.join(os.getcwd(), 'data')
@@ -46,13 +46,13 @@ inputs = {
     'filepath': filepath_data
         }
 
-#%% 2. Retrieve images
+#%% 2. Retrieve images and save
 
 # retrieve satellite images from GEE
-#metadata = SDS_download.retrieve_images(inputs)
+metadata = SDS_download.retrieve_images(inputs)
 
 # if you have already downloaded the images, just load the metadata file
-metadata = SDS_download.get_metadata(inputs) 
+#metadata = SDS_download.get_metadata(inputs) 
 
 #%% 3. Batch shoreline detection
     
@@ -75,7 +75,7 @@ settings = {
 }
 
 # [OPTIONAL] preprocess images (cloud masking, pansharpening/down-sampling)
-#SDS_preprocess.save_jpg(metadata, settings)
+SDS_preprocess.save_jpg(metadata, settings)
 
 # [OPTIONAL] create a reference shoreline (helps to identify outliers and false detections)
 settings['reference_shoreline'] = SDS_preprocess.get_reference_sl(metadata, settings)
