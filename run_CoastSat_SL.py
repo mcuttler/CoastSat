b8d4f7ea43cbf53deb3452f35f69ca6916771daa
mcuttler <39910041+mcuttler@users.noreply.github.com>
2019-06-26 11:00:31 +0800

    data for testing Muttonbird, Gnarabup, and Torbay

--- run_CoastSat_SL.py
+++ run_CoastSat_SL.py
@@ -23,16 +23,16 @@ from coastsat import SDS_download, SDS_preprocess, SDS_shoreline, SDS_tools, SDS
 #            [151.301454, -33.700754]]]
 # can also be loaded from a .kml polygon
 #kml_polygon = os.path.join(os.getcwd(), 'examples', 'NARRA_polygon.kml')
-polygon = SDS_tools.polygon_from_kml(os.path.join(os.getcwd(), 'KMLs','MANDURAH_OCEAN.kml'))
+polygon = SDS_tools.polygon_from_kml(os.path.join(os.getcwd(), 'KMLs','GNARABUP.kml'))
        
 # date range
-dates = ['1985-01-01', '2019-06-10']
+dates = ['2019-03-01', '2019-06-10']
 
 # satellite missions
-sat_list = ['L5','L7','L8','S2']
+sat_list = ['S2']
 
 # name of the site
-sitename = 'MANDURAH'
+sitename = 'GNARABUP'
 
 # filepath where data will be stored
 filepath_data = os.path.join(os.getcwd(), 'data')
@@ -49,10 +49,10 @@ inputs = {
 #%% 2. Retrieve images and save
 
 # retrieve satellite images from GEE
-#metadata = SDS_download.retrieve_images(inputs)
+metadata = SDS_download.retrieve_images(inputs)
 
 # if you have already downloaded the images, just load the metadata file
-metadata = SDS_download.get_metadata(inputs) 
+#metadata = SDS_download.get_metadata(inputs) 
 
 
 # settings for the shoreline extraction
@@ -74,8 +74,8 @@ settings = {
 }
 
 # [OPTIONAL] preprocess images (cloud masking, pansharpening/down-sampling)
-#SDS_preprocess.save_jpg(metadata, settings)
-
+SDS_preprocess.save_jpg(metadata, settings)
+#
 #%% 3. Batch shoreline detection
     
 # [OPTIONAL] create a reference shoreline (helps to identify outliers and false detections)
