47ec2331ccb6179a0707181bdf190e2f628d9bb0
mcuttler <michael.cuttler@uwa.edu.au>
2023-05-23 19:13:27 +0800

    modify example for testing

--- example.py
+++ example.py
@@ -21,30 +21,32 @@ from scipy import stats
 from datetime import datetime, timedelta
 import pytz
 from coastsat import SDS_download, SDS_preprocess, SDS_shoreline, SDS_tools, SDS_transects
-
+%matplotlib qt
+#%% 
 # region of interest (longitude, latitude in WGS84)
-# polygon = [[[151.301454, -33.700754],
-#             [151.311453, -33.702075],
-#             [151.307237, -33.739761],
-#             [151.294220, -33.736329],
-#             [151.301454, -33.700754]]]
+polygon = [[[115.751187, -31.900668],
+            [115.751181, -31.929797],
+            [115.757164, -31.929952],
+            [115.756803, -31.900845],
+            [115.751187, -31.900668]]]
+
 # can also be loaded from a .kml polygon
 # kml_polygon = os.path.join(os.getcwd(), 'examples', 'NARRA_polygon.kml')
 # polygon = SDS_tools.polygon_from_kml(kml_polygon)
 # convert polygon to a smallest rectangle (sides parallel to coordinate axes)
-# polygon = SDS_tools.smallest_rectangle(polygon)
+polygon = SDS_tools.smallest_rectangle(polygon)
 
 # # date range
-# dates = ['1984-01-01', '2022-01-01']
+dates = ['2020-01-01', '2020-03-01']
 
 # # satellite missions
-# sat_list = ['L5','L7','L8']
-# collection = 'C02' # choose Landsat collection 'C01' or 'C02'
+sat_list = ['L5','L7','L8','L9','S2']
+collection = 'C02' # choose Landsat collection 'C01' or 'C02'
 # # name of the site
-# sitename = 'NARRA'
+sitename = 'FLOREAT'
 
 # # filepath where data will be stored
-# filepath_data = os.path.join(os.getcwd(), 'data')
+filepath_data = os.path.join(os.getcwd(), 'data')
 
 # # put all the inputs into a dictionnary
 inputs = {
@@ -75,9 +77,9 @@ metadata = SDS_download.get_metadata(inputs)
 # settings for the shoreline extraction
 settings = {
     # general parameters:
-    'cloud_thresh': 0.1,        # threshold on maximum cloud cover
-    'dist_clouds': 300,         # ditance around clouds where shoreline can't be mapped
-    'output_epsg': 28356,       # epsg code of spatial reference system desired for the output
+    'cloud_thresh': 0.5,        # threshold on maximum cloud cover
+    'dist_clouds': 0,         # ditance around clouds where shoreline can't be mapped
+    'output_epsg': 28350,       # epsg code of spatial reference system desired for the output
     # quality control:
     'check_detection': False,    # if True, shows each shoreline detection to the user for validation
     'adjust_detection': False,  # if True, allows user to adjust the postion of each shoreline by changing the threhold
@@ -85,16 +87,16 @@ settings = {
     # [ONLY FOR ADVANCED USERS] shoreline detection parameters:
     'min_beach_area': 1000,     # minimum area (in metres^2) for an object to be labelled as a beach
     'min_length_sl': 500,       # minimum length (in metres) of shoreline perimeter to be valid
-    'cloud_mask_issue': False,  # switch this parameter to True if sand pixels are masked (in black) on many images
-    'sand_color': 'default',    # 'default', 'latest', 'dark' (for grey/black sand beaches) or 'bright' (for white sand beaches)
+    'cloud_mask_issue': True,  # switch this parameter to True if sand pixels are masked (in black) on many images
+    'sand_color': 'bright',    # 'default', 'latest', 'dark' (for grey/black sand beaches) or 'bright' (for white sand beaches)
     'pan_off': False,           # True to switch pansharpening off for Landsat 7/8/9 imagery
     # add the inputs defined previously
     'inputs': inputs,
 }
 
 # [OPTIONAL] preprocess images (cloud masking, pansharpening/down-sampling)
-# SDS_preprocess.save_jpg(metadata, settings)
-
+SDS_preprocess.save_jpg(metadata, settings)
+#%%
 # [OPTIONAL] create a reference shoreline (helps to identify outliers and false detections)
 settings['reference_shoreline'] = SDS_preprocess.get_reference_sl(metadata, settings)
 # set the max distance (in meters) allowed from the reference shoreline for a detected shoreline to be valid
@@ -102,9 +104,9 @@ settings['max_dist_ref'] = 100
 
 # extract shorelines from all images (also saves output.pkl and shorelines.kml)
 output = SDS_shoreline.extract_shorelines(metadata, settings)
-
+#%%
 # remove duplicates (images taken on the same date by the same satellite)
-output = SDS_tools.remove_duplicates(output)
+# output = SDS_tools.remove_duplicates(output)
 # remove inaccurate georeferencing (set threshold to 10 m)
 output = SDS_tools.remove_inaccurate_georef(output, 10)
 
